4158bc4f2fe3ba49f850b2797bca2492896d1381
Conan Bitter <conan.bitter@gmail.com>
2024-09-14 17:48:15 +0300

    Add serving static files and favicons

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,33 @@
 import fastify from 'fastify'
+import fastifyStatic from '@fastify/static'
+import path from 'node:path'
 
-const server = fastify()
+const server = fastify({ logger: true });
 
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
+server.register(fastifyStatic, {
+    root: path.join(__dirname, "../web"),
+    index: false,
+    serve: false,
+});
+
+server.register(fastifyStatic, {
+    root: path.join(__dirname, "../web/static"),
+    index: false,
+    prefix: "/static/",
+    decorateReply: false,
+});
+
+server.get('/', async (request, reply) => {
+    return reply.sendFile("index.html");
+})
+
+server.get('/favicon.ico', async (request, reply) => {
+    return reply.sendFile("favicon.ico");
 })
 
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
-        console.error(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
     }
-    console.log(`Server listening at ${address}`)
 })
