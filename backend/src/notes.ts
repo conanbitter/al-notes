4fad2d4a4162ccc82a94aeb953dbde8d7297d376
Conan Bitter <conan.bitter@gmail.com>
2024-09-14 21:41:03 +0300

    Expand getNoteTree functionality

--- backend/src/notes.ts
+++ backend/src/notes.ts
@@ -8,14 +8,19 @@ interface NoteTreeNode {
     children: NoteTreeNode[];
 }
 
-export async function getNoteTree(): Promise<NoteTreeNode[]> {
-    const list = await db.selectFrom("notes").select(["id", "title"]).execute();
-    return list.map((entry) => {
-        return {
-            id: entry.id,
-            title: entry.title,
-            opened: false,
-            children: []
-        }
-    });
+// TODO Open tree to specific note (from argument)
+export async function getNoteTree(noteId?: number): Promise<NoteTreeNode[]> {
+    if (noteId === undefined) {
+        const list = await db.selectFrom("notes").select(["id", "title"]).execute();
+        return list.map((entry) => {
+            return {
+                id: entry.id,
+                title: entry.title,
+                opened: false,
+                children: []
+            }
+        });
+    } else {
+        return []
+    }
 }
